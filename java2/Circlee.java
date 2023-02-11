4fdabfdec2b9ee6133aaf029fc65514695ac3dba
erkegalymbekova@gmail.com <erkegalymbekova@gmail.com>
2023-02-11 12:51:43 +0600

    Using the public method for calculating the area of a circle

--- java2/Circlee.java
+++ java2/Circlee.java
@@ -16,4 +16,31 @@ public Circle (double radius, String color) {
     setColor(color);
 }
 
+public Circle (double radius, String color, boolean filled) {
+    this(radius, color);
+    setFilled(filled);
+}
+
+public double getRadius() {
+    return radius; 
+}
+
+public void setRadius(double radius) {
+    this.radius = radius;
+}
+
+public double getArea() {
+    return radius * radius * Math.PI;
+}
+
+public double getPerimeter() {
+    return 2 * radius * Math.PI;
+}
+
+@Override
+public String toString() {
+    return "A Circle with radius = "+ radius
+         + ", which is a subclass of "+ super.toString();
+}
+
 }
